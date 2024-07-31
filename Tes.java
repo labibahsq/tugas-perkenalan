638d223941efd2271b043f6799f102286ca43aad
labibahsq <labibah_qothrunnada_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:50:29 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,11 @@
+public class Tes {
+   public static void main(String[] args) {
+    System.out.println("ini file Tes");
+    System.out.println("Halo");
+    System.out.println("Perkenalkan nama saya Labibah sausan Qothrunnada");
+    System.out.println("Biasa dipanggil Bibah");
+    System.out.println("Alamat saya berada di Jl.Bali Gg.1 No.2 Kota Madiun");
+    System.out.println("SMP saya di SMP Negeri 1 Madiun");
+    System.out.println("Nomor HP saya 089636249210");
+   }
+}
